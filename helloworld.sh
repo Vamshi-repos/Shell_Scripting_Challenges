53865427e7b69e2091cc151f10a5f8b27539bc16
root <root@ip-172-31-19-81.ec2.internal>
2026-09-03 11:46:26 +0000

    pushed from ec2

--- helloworld.sh
+++ helloworld.sh
@@ -1,3 +1,4 @@
 #!/bin/bash  # it tells the OS which interpreeetr to use to execuittet the script
  echo " Hello World"    
  echo "Hello Vamshi!!"
+ echo "updated a stdin from the Linux Ubunti ec2"
