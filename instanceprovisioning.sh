1e86a6d8f1e0bed8ec6ba7fd5e9b2e59e9944b9c
Vamshi-repos <chennavamshi444@gmail.com>
2026-09-04 00:45:47 +0530

    pushing new code

--- instanceprovisioning.sh
+++ instanceprovisioning.sh
@@ -6,6 +6,6 @@ AMI_ID="ami-0b6d9d3d33ba97d99"
 
 for instance in $@
 do
-    aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$instance'}]' \ 
+    aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name, \ Value='$instance'}]' \ 
     --query 'Reservation[0].Instances[0].PrivateIpAddress' --output text
 done
