8c902a195124855841e9f4abfd1c84a6229247d3
Vamshi-repos <chennavamshi444@gmail.com>
2026-09-04 23:15:00 +0530

    pushing new code

--- conf-prov_mongodb.sh
+++ conf-prov_mongodb.sh
@@ -9,7 +9,7 @@ G="\e[32m"
 
 
 if [ $USERID -ne 0 ]; then
-   echo -d "$R Please run  the sccript with ROOT user $N" | tee -a $LOGS_FILE
+   echo -e "$R Please run  the sccript with ROOT user $N" | tee -a $LOGS_FILE
    exit 1
 fi 
 
@@ -17,10 +17,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then
-       echo -e "$2 ..... $3 FAILURE $N" | tee -a $LOGS_FILE
+       echo -e "$2 ..... $R FAILURE $N" | tee -a $LOGS_FILE
        exit 1
     else
-       echo -e " $2 ..... $G SUCCESS $N" | tee -a $LOGS_FILE
+       echo -e "$2 ..... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
@@ -31,15 +31,15 @@ VALIDATE $? "Copying Package Installation REPOS"
 dnf install mongodb-org -y &>>$LOGS_FILE
 VALIDATE $? "Installing Mongodb Server"
 
-systemctl enable mongodb &>>$LOGS_FILE
+systemctl enable mongod &>>$LOGS_FILE
 VALIDATE $? "Enable Mongodb service"
 
-systemctl start mongodb &>>$LOGS_FILE
+systemctl start mongod &>>$LOGS_FILE
 VALIDATE $? "Started ting Mongodb service"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "updating mongod conf file to alloW all connections from default 127.0.0.1 to 0.0.0.0"
 
-systemctl restart mongodb 
+systemctl restart mongod
 VALIDATE $? "Restarted  Mongodb service"
 
